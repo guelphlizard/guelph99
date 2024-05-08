d1cc85f3c62c59fdc6ef1fee368d1cdfdc6913bd
lizardN <blutech02@gmail.com>
2024-05-08 23:12:10 +0200

    first commit

---
+++ models/product.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    status:{type:String, required:true},
+    type: {type: String, required:true },
+    item: { type: String, required:true },
+    code: {type: String, required: true},
+    account: {type: String, required: true},
+    cogs: {type: String},
+    assetAccount: {type: String},
+    accumulatedDepreciation: {type: Number},
+    purchaseDescription: {type:String,},
+    qty: {type: Number, required: true},
+    cost: {type: Number, required: true},
+    price: {type: Number, required: true},
+    grossPrice: {type: Number},
+    amountsIncludeVat: {type: String},
+    purchasedForResale: {type: String},
+ 
+});
+
+module.exports = mongoose.model('Product', schema);
